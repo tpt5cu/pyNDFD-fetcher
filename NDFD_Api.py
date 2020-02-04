5211c39a614f1f6766214c00f66ea2b6a8fb00c5
Tuomas Talvitie <ttalvitie49@gmail.com>
2020-02-04 13:55:09 -0700

    API changed, date now parsed correctly. XML to dict still in progress

--- NDFD_Api.py
+++ NDFD_Api.py
@@ -11,6 +11,7 @@ import datetime
 import requests
 from urllib.parse import urlencode, quote
 import xml.etree.ElementTree as ET
+import xmltodict
 
 
 # start = datetime.datetime.today() + datetime.timedelta(days=1)
@@ -23,17 +24,26 @@ import xml.etree.ElementTree as ET
 Single Point Unsummarized Data: Returns DWML-encoded NDFD data for a point
 """
 #Works
-def singlePointDataQuery(lat1, lon1, product, begin, end, optional_params=['wspd', 'wdir']):
+def singlePointDataQuery(lat1, lon1, product, begin, end, Unit='m', optional_params=['wspd', 'wdir']):
 	params = {
 		'lat':lat1,
 		'lon':lon1,
-		'product':product,
-		'begin':begin,
-		'end':end,
+		'product':product
 	}
+	params2 = {'begin':begin,
+	'end':end
+	}
+	params3 = {
+		'Unit':Unit,
+	}
+	urlString = urlencode(params)
+	subString = ''
+	for key, value in params2.items():
+		subString += '&'+str(key) + '=' + str(value)
+	urlString+=subString
 	for i in optional_params:
-		params[i] = i
-
+		params3[i] = i
+	urlString +='&' + urlencode(params3)
 
 	return urlencode(params)
 
@@ -42,16 +52,26 @@ Multiple Point Unsummarized Data: Returns DWML-encoded NDFD data for a list of p
 """
 
 #Works, but need to parse the list of coords legibly first. But API does work.
-def listPointDataQuery(product, begin, end, listLatLon=('38.99,-77.02 39.70,-104.80'), optional_params=['wspd', 'wdir']):
+def listPointDataQuery(product, begin, end, listLatLon=('38.99,-77.02 39.70,-104.80'), Unit='m', optional_params=['wspd', 'wdir']):
 	params = {
 		'listLatLon':listLatLon,
-		'product':product,
-		'begin':begin,
-		'end':end,
+		'product':product
 	}
-	for i in optional_params:
-		params[i] = i
 
+	params2 = {'begin':begin,
+	'end':end
+	}
+	params3 = {
+		'Unit':Unit,
+	}
+	urlString = urlencode(params)
+	subString = ''
+	for key, value in params2.items():
+		subString += '&'+str(key) + '=' + str(value)
+	urlString+=subString
+	for i in optional_params:
+		params3[i] = i
+	urlString +='&' + urlencode(params3)
 
 	return urlencode(params)
 
@@ -60,19 +80,29 @@ Unsummarized Data for a Subgrid:
  Returns DWML-encoded NDFD data for a subgrid defined by a lower left and upper right point
 """
 ###Deprecated, examples online dont work
-def subGridDataQuery(lat1, lon1, lat2, lon2, resolutionSub, product, begin, end, optional_params=['wspd', 'wdir']):
+def subGridDataQuery(lat1, lon1, lat2, lon2, resolutionSub, product, begin, end, Unit='m', optional_params=['wspd', 'wdir']):
 	params = {
 		'lat1':lat1,
 		'lon1':lon1,
 		'lat2':lat2,
 		'lon2':lon2,
 		'resolutionSub':resolutionSub,
-		'product':product,
-		'begin':begin,
-		'end':end,
+		'product':product
 	}
+	params2 = {'begin':begin,
+	'end':end
+	}
+	params3 = {
+		'Unit':Unit,
+	}
+	urlString = urlencode(params)
+	subString = ''
+	for key, value in params2.items():
+		subString += '&'+str(key) + '=' + str(value)
+	urlString+=subString
 	for i in optional_params:
-		params[i] = i
+		params3[i] = i
+	urlString +='&' + urlencode(params3)
 
 	return urlencode(params)
 
@@ -120,26 +150,54 @@ Returns DWML-encoded NDFD data for a rectangle defined by a center point and
 distances in the latitudinal and longitudinal directions.
 """
 
-def subGrid(centerPointLat, centerPointLon, distanceLat, distanceLon, resolutionSquare, product, begin, end, optional_params=['wspd', 'wdir']):
+def subGrid(centerPointLat, centerPointLon, distanceLat, distanceLon, resolutionSquare, product, begin, end, Unit='m', optional_params=['wspd', 'wdir']):
+	#Split into 3 dictionaries, each are encoded in a different manner
 	params = {
 		'centerPointLat':centerPointLat,
 		'centerPointLon':centerPointLon,
 		'distanceLat':distanceLat,
 		'distanceLon':distanceLon,
 		'resolutionSquare':resolutionSquare,
-		'product':product,
-		'begin':begin,
-		'end':end,
+		'product':product
 	}
+	#Begin/end has special encoding
+	params2 = {'begin':begin,
+	'end':end
+	}
+	params3 = {
+		'Unit':Unit,
+	}
+
+	urlString = urlencode(params)
+	subString = ''
+	for key, value in params2.items():
+		subString += '&'+str(key) + '=' + str(value)
+	urlString+=subString
 	for i in optional_params:
-		params[i] = i
-	return urlencode(params)
+		params3[i] = i
+	urlString +='&' + urlencode(params3)
 
+	return urlString
 
+#For daywise
+def subGridDayWise(centerPointLat, centerPointLon, distanceLat, distanceLon, resolutionSquare, startDate, numDays):
+	params = {
+		'centerPointLat': centerPointLat,
+		'centerPointLon': centerPointLon,
+		'distanceLat': distanceLat,
+		'distanceLon': distanceLon,
+		'resolutionSquare': resolutionSquare,
+		'startDate': startDate,
+		'numDays':numDays
+	}
+	return urlencode(params, doseq=True)
 
-# raise Exception
+#Main URL path
 def _url(path=''):
     return 'http://www.weather.gov/forecasts/xml/sample_products/browser_interface/ndfdXMLclient.php?' + path
+
+
+
 
 #Still in dev, now returns values
 def parseXml(data):
@@ -147,10 +205,14 @@ def parseXml(data):
 	print(root)
 	# for child in root.iter('*'):
  #   		print(child.tag, child.attrib)
-	for child in root.iter('*'):
-		print(child.tag, child.text)
-	for i in root.xpath("//time-layout"):
-		print(i)
+	# for child in root.iter('*'):
+	# 	print(child.tag, child.text)
+	# for i in root.xpath("//time-layout"):
+	# 	print(i)
+	d = xmltodict.parse(data.content)	
+	dataDict = d['dwml']['data']
+	for key, val in dataDict.items():
+		print(key, val)
 	return 
 
 
@@ -165,32 +227,36 @@ def run_request(q):
 	return resp
 
 #test each API call
-def run_request_tests(_tests):
+def run_tests(_tests):
 	for i in _tests.keys():
 		try:
 			run_request(_tests[i])
 			print('*************** QUERY ', i, " HAS SUCCEEDED ************")
 		except:
+			print('*************** QUERY ', i, " FAILED ************")
 			print("en error ocurred")
 			e = sys.exc_info()[0]
 			print(e)
 
 
 _tests={
-	'q1':subGridDataQuery('35.00', '-82.00', '35.5', '-81.50', '20.0', 'time-series', '2020-02-01T17:00:00', '2020-02-02T18:00:00'),
-	'q2':singlePointDataQuery('37.33', '-122.03','time-series', '2020-02-01T17:12:35', '2020-06-02T17:12:35'),
-	'q3':listPointDataQuery('time-series', '2020-02-01T17:12:35', '2020-06-02T17:12:35'),
+	'q1':singlePointDataQuery('37.33', '-122.03','time-series', '2020-02-01T17:12:35', '2020-06-02T17:12:35'),
+	'q2':listPointDataQuery('time-series', '2020-02-01T17:12:35', '2020-06-02T17:12:35'),
+	'q3':subGridDataQuery('35.00', '-82.00', '35.5', '-81.50', '20.0', 'time-series', '2020-02-09T17:00:00', '2020-02-09T18:00:00'),
 	'q4':listCoordsInGrid('35.00', '-82.00', '35.50', '-81.50', '20.0'),
 	'q5':getDataZipcode('20190 25414', 'time-series', '2020-02-01T17:12:35', '2020-06-02T17:12:35' ),
-	'q6':subGrid('38.0', '-97.4', '50.0', '50.0', '20.0' ,'time-series','2020-02-01T17:00:00','2020-02-01T18:00:00')
+	# 'q6':subGrid('38.0', '-97.4', '4.0', '4.0', '0.001' ,'time-series','2020-02-01T17:00:00','2020-02-01T18:00:00'),
+	'q7':subGrid('40.7128', '-74.0060', '1.0', '1.0', '1.0' ,'time-series','2020-02-04T20:00:00','2020-02-04T20:00:00'),
+	# 'q8':subGridDayWise('38.0', '-97.4', '5.0', '5.0', '5.0', '2020-02-04', '1')
 	}
 
 
 
 if __name__ == '__main__':
-	data=run_request(_tests['q2'])
-	# run_request_tests(_tests)
-	parseXml(data)
+	# data=run_request(_tests['q7'])
+	# print(data.content)
+	# run_tests(_tests)
+	# parseXml(data)
 
 
 
